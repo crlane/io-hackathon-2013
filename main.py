f14d43f79f65a7d3540ffd1cf4d2db35be0bd046
Joseph Reed <joseph.reed@outlook.com>
2013-05-17 00:01:28 -0400

    Have the logic for the twitter search and parse.
    Not 100% sure how to plug this into the webapp2. Will keep researching
    same.

--- main.py
+++ main.py
@@ -15,10 +15,33 @@
 # limitations under the License.
 #
 import webapp2
+import urllib2
+import json
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('<h1>Welcome to Google I/O Extended Atlanta 2013</h1>')
+
+#JR - Here is what I came up with for the twitter search and parse. I’m not sure how
+#to plug this into the webapp2. I’ll keep plugging away. Thanks for setting this up.
+#Joe Reed - 05/16/13
+#TwitterSearchURL = "http://search.twitter.com/search.json?q=%40mollywood"
+#
+#response = json.load(urllib2.urlopen(TwitterSearchURL))
+#
+#NumberOfTweets = len(response['results'])
+#
+#if NumberOfTweets < 1:
+#	print "No Tweets Found..."
+#else:
+#	for x in range(0, NumberOfTweets):
+#		try:
+#			print response['results'][x]['from_user']
+#			print response['results'][x]['from_user_name']
+#			print response['results'][x]['created_at']
+#			print response['results'][x]['text']
+#		except:
+#			print "Error while trying to print Tweets..."
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
