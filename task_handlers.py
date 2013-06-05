2cc2e2be568c62bf49c1d441744b5d3411d4d969
JR Rickerson <jrrickerson@gmail.com>
2013-06-04 21:09:09 -0400

    Add basic "Task" backend for Twitter polling.
    Twitter polling task should only allow calls from an admin user, and
    only from a backend instance, so we can ensure users can't call it.  The
    scheduled task will call the URL as the admin user.

--- task_handlers.py
+++ task_handlers.py
@@ -1,6 +1,32 @@
 import webapp2
+import json
+import urllib2
+import logging as log
+
+from google.appengine.api import backends
+
+twitter_search_url = "http://search.twitter.com/search.json?q=%40{0}"
 
 class TwitterTaskHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Updated.');
+        if not backends.get_backend():
+            self.error(403)
+            return
+        count = self._search_twitter('googleio')
+        self.response.write('Updated. Got {0} tweets.'.format(count));
 
+    def _search_twitter(self, search_term):
+        url = twitter_search_url.format(search_term)
+
+        tweets = []
+        try:
+            response = json.load(urllib2.urlopen(url))
+            tweets = response['results']
+        except ValueError:
+            log.error('Could not parse Twitter search response')
+        except KeyError:
+            log.error('No field "results" in Twitter response')
+
+        tweet_count = len(tweets)
+        log.info('Found {0} matching tweets'.format(tweet_count))
+        return tweet_count
