e2212f9bd7f6428cb0d77a328babde02d5e07fe8
Cameron Lane <lane.cameron@gmail.com>
2013-07-04 22:00:24 -0400

    added static file handling configuration; template boilerplate

--- handlers.py
+++ handlers.py
@@ -1,7 +1,14 @@
 import webapp2
+import jinja2
+
+_JINJA = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('templates/'),
+        extensions=['jinja2.ext.autoescape'])
 
 class IndexHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('<h1>Welcome to Google I/O Extended Atlanta 2013</h1>')
+        msg = 'Welcome to Google I/O Extended Atlanta 2013'
+        template = _JINJA.get_template('index.html')
+        self.response.write(template.render(message=msg))
 
 
