f093016818bd2da9048782f989d9deaa1c6bf5e7
Cameron Lane <lane.cameron@gmail.com>
2013-07-06 12:05:49 -0400

    added handler and template for twitter visualization; rudimentary implementation of drawing

--- handlers.py
+++ handlers.py
@@ -1,5 +1,7 @@
 import webapp2
 import jinja2
+import string
+import json
 
 _JINJA = jinja2.Environment(
         loader=jinja2.FileSystemLoader('templates/'),
@@ -11,4 +13,9 @@ class IndexHandler(webapp2.RequestHandler):
         template = _JINJA.get_template('index.html')
         self.response.write(template.render(message=msg))
 
-
+class TwitterDisplayHandler(webapp2.RequestHandler):
+    def get(self):
+        # right now, twitter_data is a dict
+        twitter_data = json.dumps(dict(zip([a for a in string.ascii_lowercase[:10]], xrange(2,12))))
+        template = _JINJA.get_template('twitterdisplay.html')
+        self.response.write(template.render(twitter_data = twitter_data))
